c85443f3650e574538c9a12790b5f06d3c19f1f4
Yasin Abbasi <yasinabbasi876@gmail.com>
2025-11-25 11:39:40 +0330

    Delete devops-vm/scripts directory

--- devops-vm/scripts/db-load.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# simple mysqlslap load example
-MYSQL_USER=${1:-wp}
-MYSQL_PASS=${2:-wppass}
-MYSQL_DB=${3:-wordpress}
-
-mysqlslap --user=$MYSQL_USER --password=$MYSQL_PASS --host=127.0.0.1 --concurrency=50 --iterations=10 --query="SELECT SLEEP(0.05)" --create-schema=$MYSQL_DB
